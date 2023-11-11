a526a10fb350b820d5cf0318a12e87ef3872861f
Siddhesh <sidd.mhapankar7@gmail.com>
2023-11-11 12:43:46 +0530

    Updated Commit

--- A1_Concept/Week1_Maths/Q4_Palindrome.java
+++ A1_Concept/Week1_Maths/Q4_Palindrome.java
@@ -1,29 +1,30 @@
 package A1_Concept.Week1_Maths;
+
 public class Q4_Palindrome {
-        public static boolean isPalindrome(String str) {
-            int left = 0;
-            int right = str.length() - 1;
-            
-            while (left < right) {
-                if (str.charAt(left) != str.charAt(right)) {
-                    return false;
-                }
-                
-                left++;
-                right--;
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
             }
-            
-            return true;
+
+            left++;
+            right--;
         }
-        
-        public static void main(String[] args) {
-            String word = "racecar";
-            
-            if (isPalindrome(word)) {
-                System.out.println("The word is a palindrome.");
-            } else {
-                System.out.println("The word is not a palindrome.");
-            }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word = "racecar";
+
+        if (isPalindrome(word)) {
+            System.out.println("The word is a palindrome.");
+        } else {
+            System.out.println("The word is not a palindrome.");
         }
-       
+    }
+
 }
